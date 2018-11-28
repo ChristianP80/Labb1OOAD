9a122df9606253eed910c007b22c736b336c23d6
Christian Persson <chrissp_@hotmail.com>
2018-11-28 08:17:18 +0100

    program done, command pattern left to implement

---
+++ Labb1OOAD/ViewModels/ListOfInfo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Labb1OOAD.ViewModels
+{
+    public class ListOfInfo : INotifyPropertyChanged
+    {
+        private string _someText;
+        public string SomeText
+        {
+            get { return _someText; }
+            set { SetProperty(ref _someText, value); }
+        }
+
+        private ObservableCollection<string> _listOfSomeText = new ObservableCollection<string>();
+        public ICommand SaveCommand { get; private set; }
+
+        public ObservableCollection<string> ListOfSomeText
+        {
+            get { return _listOfSomeText; }
+            private set { _listOfSomeText = value; }
+
+        }
+
+        public ListOfInfo()
+        {
+            SaveCommand = new Command(
+                execute: SaveEntryText,
+                canExecute: obj => { return true; }
+            );
+        }
+
+        private void SaveEntryText(Object obj)
+        {
+            _listOfSomeText.Add(SomeText);
+            SomeText = "";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Object.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
